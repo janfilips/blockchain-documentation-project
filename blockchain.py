f614e5dc5e6974336d03910b802eab105aa8ba4a
ernie <ernie@Jans-MacBook-Pro.local>
2018-03-18 19:39:32 +0100

    drafted out basic blockchain functions for managing the chain

---
+++ blockchain.py
@@ -0,0 +1,23 @@
+class blockchain(object):
+    def __init__(self):
+        self.chain = []
+        self.current_transactions = []
+    
+    def new_block(self):
+        # creates a new block and adds it on the chain
+        pass
+    
+    def add_new_transaction(self):
+        # adds a transaction into the list of transactions
+        pass
+
+    @staticmethod
+    def hash(block);
+        # hashes a block
+        pass
+
+    @property
+    def last_block(self):
+        # returns last block in the chain
+        pass
+
