262f557a182d38db04c354ceb1b51ad0a0fc6547
ernie <ernie@Jans-MacBook-Pro.local>
2018-03-20 00:25:38 +0100

    service commit, code tyding up..

--- blockchain.py
+++ blockchain.py
@@ -1,23 +1,33 @@
-class blockchain(object):
+class BlockChain(object):
+    """ Mwah."""
     def __init__(self):
         self.chain = []
         self.current_transactions = []
-    
-    def new_block(self):
-        # creates a new block and adds it on the chain
-        pass
-    
-    def add_new_transaction(self):
-        # adds a transaction into the list of transactions
-        pass
 
     @staticmethod
-    def hash(block);
+    def hash(block):
         # hashes a block
+        pass
+
+    def new_block(self):
+        # creates a new block and adds it on the chain
         pass
 
     @property
     def last_block(self):
         # returns last block in the chain
+        return self.chain[-1]
+
+    def full_chain(self):
+        # returns the full chain and a number of blocks
         pass
 
+    def add_new_transaction(self, sender, recipient, data):
+        # adds a new transaction into the list of transactions
+        # these transactions go into the next mined block
+        self.current_transactions.append({
+            "sender":sender,
+            "recient":recipient,
+            "data":data,
+        })
+        return int(self.last_block['index'])+1
